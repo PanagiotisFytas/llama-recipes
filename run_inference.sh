1bc4d4cba606642fce98a17590c0873dac076ad6
Panagiotis Fytas <p.fytas@datahow.ch>
2024-10-14 17:24:19 +0100

    add script for finetune and inference

--- run_inference.sh
+++ run_inference.sh
@@ -4,4 +4,5 @@ In summary, the genotype distributions of the  AGT  M235T polymorphism influence
     --model_name 'meta-llama/Llama-3.2-1B-Instruct' \
     --peft_model './my_lora_weights/biotrip_json_no_quant_weight_decay' \
     --max_new_tokens 512 \
+    --temperature 0
 #    --prompt_file './test_biotriplex_prompt.txt'
