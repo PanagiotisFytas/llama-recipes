46ac044ddcc95539caa3c8ef3eb773a7636a9ef1
Panagiotis Fytas <p.fytas@datahow.ch>
2024-10-15 17:52:16 +0100

    add script for finetune and inference

--- run_inference_large.sh
+++ run_inference_large.sh
@@ -10,6 +10,7 @@
 python recipes/quickstart/inference/local_inference/inference.py \
     --model_name 'meta-llama/Llama-3.1-8B-Instruct' \
     --peft_model './my_lora_weights/3.1_large_biotrip_json_no_quant_high_weight_decay' \
+    --quantization '4bit' \
     --max_new_tokens 4024 \
     --top_p 1 \
     --do_sample False \
