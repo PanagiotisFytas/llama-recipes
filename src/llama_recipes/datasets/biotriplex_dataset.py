467586fbc139b4ab2d6a1664e32289560d7cb2f9
Panagiotis Fytas <p.fytas@datahow.ch>
2024-10-15 16:13:41 +0100

    add script for finetune and inference

--- src/llama_recipes/datasets/biotriplex_dataset.py
+++ src/llama_recipes/datasets/biotriplex_dataset.py
@@ -22,7 +22,7 @@ def triplets_to_json(triplets):
 INSTRUCTION = """Given a text, extract the gene-disease-relation triplets in a json format."""
 
 class BioTriplexDataset(Dataset):
-    def __init__(self, dataset_config, tokenizer, split_name, max_words=512):
+    def __init__(self, dataset_config, tokenizer, split_name, max_words=None):
         #self.data = json.load(open(dataset_config.data_path))
 
         if split_name == "train":
@@ -69,12 +69,13 @@ class BioTriplexDataset(Dataset):
         example.append(self.tokenizer.eos_token_id)
         example = torch.tensor(example, dtype=torch.int64)
         # self.longest_input = max(self.longest_input, example.shape[0])
-        padding = self.max_words - example.shape[0]
-        if padding > 0:
-            example = torch.cat((example, torch.zeros(padding, dtype=torch.int64) - 1))
-        elif padding < 0:
-            example = example[: self.max_words]
-            # self.num_truncated_examples += 1
+        if self.max_words is not None:
+            padding = self.max_words - example.shape[0]
+            if padding > 0:
+                example = torch.cat((example, torch.zeros(padding, dtype=torch.int64) - 1))
+            elif padding < 0:
+                example = example[: self.max_words]
+                # self.num_truncated_examples += 1
         labels = copy.deepcopy(example)
         labels[: len(prompt)] = -1
         example_mask = example.ge(0)
@@ -82,7 +83,7 @@ class BioTriplexDataset(Dataset):
         example[~example_mask] = 0
         labels[~label_mask] = IGNORE_INDEX
         example_mask = example_mask.float()
-        label_mask = label_mask.float()
+        # label_mask = label_mask.float()
         # example[example == -100] = self.tokenizer.pad_token_id
         # labels[labels == -100] = self.tokenizer.pad_token_id
 
