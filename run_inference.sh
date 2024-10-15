95815bdb400b94b376fa4a1a77fdc7dcd7812083
Panagiotis Fytas <p.fytas@datahow.ch>
2024-10-15 16:25:23 +0100

    add script for finetune and inference

--- run_inference.sh
+++ run_inference.sh
@@ -1,15 +1,17 @@
-echo "### Instruction:
-Given a text, extract the gene-disease-relation triplets in a json format.
-
-### Input:
-
-In summary, the genotype distributions of the  AGT  M235T polymorphism influenced the risk of essential hypertension in south Indian women and  ACE  DD is a risk in south Indian male population. MDR analysis reveals that if both TT and DD genotypes are present, prevalence of EHT is higher in the present study. The haplotype based MDR analysis suggests that we had adequate power to detect the functional relationship of the best factor model, increasing the risk of essential hypertension associated with combined genetic variations. This is the first report to evaluate the simultaneous association of  ACE,  AGTR1, and  AGT  gene polymorphisms in essential hypertension by haplotype based analyses and gender specific association in south India. Future studies are required to consider the joint effects of several candidate genes to dissect the genetic framework and gender specific association of essential hypertension.
-
-
-### Response:" | python recipes/quickstart/inference/local_inference/inference.py \
+#echo "### Instruction:
+#Given a text, extract the gene-disease-relation triplets in a json format.
+#
+#### Input:
+#
+#In summary, the genotype distributions of the  AGT  M235T polymorphism influenced the risk of essential hypertension in south Indian women and  ACE  DD is a risk in south Indian male population. MDR analysis reveals that if both TT and DD genotypes are present, prevalence of EHT is higher in the present study. The haplotype based MDR analysis suggests that we had adequate power to detect the functional relationship of the best factor model, increasing the risk of essential hypertension associated with combined genetic variations. This is the first report to evaluate the simultaneous association of  ACE,  AGTR1, and  AGT  gene polymorphisms in essential hypertension by haplotype based analyses and gender specific association in south India. Future studies are required to consider the joint effects of several candidate genes to dissect the genetic framework and gender specific association of essential hypertension.
+#
+#
+#### Response:"
+python recipes/quickstart/inference/local_inference/inference.py \
     --model_name 'meta-llama/Llama-3.2-3B-Instruct' \
     --peft_model './my_lora_weights/biotrip_json_no_quant_weight_decay' \
-    --max_new_tokens 512 \
-    --temperature 0.01 \
+    --max_new_tokens 4024 \
+    --top_p 1 \
+    --do_sample False \
+    --share_gradio True \
     --enable_salesforce_content_safety False
-#    --prompt_file './test_biotriplex_prompt.txt'
