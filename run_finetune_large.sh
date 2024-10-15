cb7fc5869859a654604a0d669ad68f4141d5f723
Panagiotis Fytas <p.fytas@datahow.ch>
2024-10-15 12:20:34 +0100

    add script for finetune and inference

--- run_finetune_large.sh
+++ run_finetune_large.sh
@@ -1,8 +1,8 @@
 python recipes/quickstart/finetuning/finetuning.py \
     --use_peft \
     --peft_method lora \
-    --model_name 'meta-llama/Llama-3.2-3B-Instruct' \
-    --output_dir './my_lora_weights/large_biotrip_json_no_quant_weight_decay' \
+    --model_name 'meta-llama/Llama-3.1-8B-Instruct' \
+    --output_dir './my_lora_weights/3.1_large_biotrip_json_no_quant_weight_decay' \
     --batch_size_training 1 \
     --weight_decay 0.2 \
     --num_epochs 6 \
