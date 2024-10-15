c2eeb527797c3b3476c9a8e4ee78909eb2b54fff
Panagiotis Fytas <p.fytas@datahow.ch>
2024-10-15 15:47:32 +0100

    add script for finetune and inference

--- src/llama_recipes/datasets/biotriplex_dataset.py
+++ src/llama_recipes/datasets/biotriplex_dataset.py
@@ -46,16 +46,16 @@ class BioTriplexDataset(Dataset):
 
         self.max_words = max_words
         self.tokenizer = tokenizer
-        self.num_truncated_examples = 0
-        self.longest_input = 0
-        self.input_seen = set()
+        # self.num_truncated_examples = 0
+        # self.longest_input = 0
+        # self.input_seen = set()
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, index):
         IGNORE_INDEX = -100  # The default setting in CrossEntropyLoss
-        self.input_seen.add(index)
+        # self.input_seen.add(index)
 
         item = self.data[index]
 
@@ -68,13 +68,13 @@ class BioTriplexDataset(Dataset):
         example = self.tokenizer.encode(example)
         example.append(self.tokenizer.eos_token_id)
         example = torch.tensor(example, dtype=torch.int64)
-        self.longest_input = max(self.longest_input, example.shape[0])
+        # self.longest_input = max(self.longest_input, example.shape[0])
         padding = self.max_words - example.shape[0]
         if padding > 0:
             example = torch.cat((example, torch.zeros(padding, dtype=torch.int64) - 1))
         elif padding < 0:
             example = example[: self.max_words]
-            self.num_truncated_examples += 1
+            # self.num_truncated_examples += 1
         labels = copy.deepcopy(example)
         labels[: len(prompt)] = -1
         example_mask = example.ge(0)
@@ -101,7 +101,7 @@ if __name__ == "__main__":
     tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.2-1B-Instruct")
     from llama_recipes.configs.datasets import biotriplex_dataset
     dataset_config = biotriplex_dataset
-    dataset = BioTriplexDataset(dataset_config, tokenizer, "train", max_words=10000)
+    dataset = BioTriplexDataset(dataset_config, tokenizer, "train", max_words=4100)
     print(dataset[0]["input_ids"].shape)
     print(dataset[0]["labels"].shape)
     print(dataset[0]["attention_mask"].shape)
