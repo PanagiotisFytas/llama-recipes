4a009780cd57e5a5d1d99d03dc88ff9b24fcdc98
Panagiotis Fytas <p.fytas@datahow.ch>
2024-10-15 16:50:11 +0100

    add script for finetune and inference

--- run_finetune.sh
+++ run_finetune.sh
@@ -4,6 +4,7 @@ python recipes/quickstart/finetuning/finetuning.py \
     --model_name 'meta-llama/Llama-3.2-3B-Instruct' \
     --output_dir './my_lora_weights/biotrip_json_no_quant_weight_decay' \
     --batch_size_training 1 \
+    --batching_strategy "padding" \
     --weight_decay 0.2 \
     --num_epochs 6 \
     --dataset biotriplex_dataset \
