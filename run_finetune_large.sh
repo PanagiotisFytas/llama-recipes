8538b226c60da3b20fc1f75afa4c4b8ae8dd185e
Panagiotis Fytas <p.fytas@datahow.ch>
2024-10-15 15:33:28 +0100

    add script for finetune and inference

--- run_finetune_large.sh
+++ run_finetune_large.sh
@@ -2,9 +2,12 @@ python recipes/quickstart/finetuning/finetuning.py \
     --use_peft \
     --peft_method lora \
     --model_name 'meta-llama/Llama-3.1-8B-Instruct' \
-    --output_dir './my_lora_weights/3.1_large_biotrip_json_no_quant_weight_decay' \
+    --output_dir './my_lora_weights/3.1_large_biotrip_json_no_quant_high_weight_decay' \
     --batch_size_training 1 \
-    --weight_decay 0.2 \
+    --weight_decay 0.5 \
     --num_epochs 6 \
     --dataset biotriplex_dataset
     #    --context_length 1024 \
+
+
+    # best val loss is 0.0556
