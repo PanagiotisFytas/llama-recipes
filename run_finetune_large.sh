d97a441480eab45dc8878970b17c20090ef797cb
Panagiotis Fytas <p.fytas@datahow.ch>
2024-10-22 00:10:52 +0100

    dis

--- run_finetune_large.sh
+++ run_finetune_large.sh
@@ -7,11 +7,11 @@ python -m torch.distributed.launch recipes/quickstart/finetuning/finetuning.py \
     --output_dir './my_lora_weights/3.1_8b_biotrip_json_no_quant_high_weight_decay_long_context' \
     --batch_size_training 1 \
     --batching_strategy "padding" \
-    --weight_decay 0.4 \
+    --weight_decay 0.2 \
     --num_epochs 6 \
     --dataset biotriplex_dataset \
     --context_length 21000 \
-    --quantization '4bit' \
+    --quantization '8bit' \
     --enable_fsdp
 
 
