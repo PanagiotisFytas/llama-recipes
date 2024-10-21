2f9c7a2690a8645381f93f81dd90609153673175
Panagiotis Fytas <p.fytas@datahow.ch>
2024-10-21 17:52:41 +0100

    validation inference

--- run_finetune_large.sh
+++ run_finetune_large.sh
@@ -1,4 +1,4 @@
-export PYTORCH_CUDA_ALLOC_CONF="expandable_segments:True"
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 python recipes/quickstart/finetuning/finetuning.py \
     --use_peft \
     --peft_method lora \
