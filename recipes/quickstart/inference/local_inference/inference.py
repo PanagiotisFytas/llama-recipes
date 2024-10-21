11632700bb39258fd976bc2c66986e474417f37c
Panagiotis Fytas <p.fytas@datahow.ch>
2024-10-21 15:26:56 +0100

    validation inference

--- recipes/quickstart/inference/local_inference/inference.py
+++ recipes/quickstart/inference/local_inference/inference.py
@@ -153,7 +153,7 @@ def main(
         from llama_recipes.datasets.biotriplex_dataset import BioTriplexDataset
         from llama_recipes.configs.datasets import biotriplex_dataset
         import json
-        dataset = BioTriplexDataset(biotriplex_dataset, tokenizer, "val", max_words=5000)
+        dataset = BioTriplexDataset(biotriplex_dataset, tokenizer, "val", max_words=50000)
         for doc_key, prompt in tqdm.tqdm(dataset.get_all_input_prompts().items()):
             output = inference(prompt, temperature, top_p, top_k, max_new_tokens, length_penalty)
             outputs[doc_key] = output
