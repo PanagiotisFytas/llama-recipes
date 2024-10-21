3a95d83ae68cf647eb81d1a130489d4dd3fcc4ca
Panagiotis Fytas <p.fytas@datahow.ch>
2024-10-21 23:42:16 +0100

    dis

--- run_finetune_large.sh
+++ run_finetune_large.sh
@@ -3,14 +3,14 @@
 python -m torch.distributed.launch recipes/quickstart/finetuning/finetuning.py \
     --use_peft \
     --peft_method lora \
-    --model_name 'meta-llama/Llama-3.2-3B-Instruct' \
-    --output_dir './my_lora_weights/3.2_3b_biotrip_json_no_quant_high_weight_decay_long_context' \
+    --model_name 'meta-llama/Llama-3.1-8B-Instruct' \
+    --output_dir './my_lora_weights/3.1_8b_biotrip_json_no_quant_high_weight_decay_long_context' \
     --batch_size_training 1 \
     --batching_strategy "padding" \
     --weight_decay 0.2 \
     --num_epochs 6 \
     --dataset biotriplex_dataset \
-    --context_length 20000 \
+    --context_length 21000 \
     --quantization '4bit' \
     --enable_fsdp
 
