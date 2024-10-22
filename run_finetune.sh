c92e35c88ef65f3e9c8fa6739e9c34b1c040ffe5
Panagiotis Fytas <p.fytas@datahow.ch>
2024-10-22 15:06:35 +0100

    eval long context

--- run_finetune.sh
+++ run_finetune.sh
@@ -1,14 +1,14 @@
-export 'PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True'
-export 'PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:128'
+#export 'PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True'
+#export 'PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:128'
 python recipes/quickstart/finetuning/finetuning.py \
     --use_peft \
     --peft_method lora \
-    --model_name 'meta-llama/Llama-3.2-1B-Instruct' \
-    --output_dir './my_lora_weights/3.2-1B_biotrip_new_template' \
+    --model_name 'meta-llama/Llama-3.2-8B-Instruct' \
+    --output_dir './my_lora_weights/3.2-8B_biotrip_new_template' \
     --batch_size_training 1 \
     --batching_strategy "padding" \
     --weight_decay 0.2 \
     --num_epochs 6 \
     --dataset biotriplex_dataset \
-    --context_length 21000 \
+    --context_length 10000 \
     --quantization '4bit'
