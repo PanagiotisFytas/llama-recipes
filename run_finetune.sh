7bc764c444fb16e9c13a25d05e7e14dc418c9792
Panagiotis Fytas <p.fytas@datahow.ch>
2024-10-22 14:57:20 +0100

    eval long context

--- run_finetune.sh
+++ run_finetune.sh
@@ -1,11 +1,12 @@
 python recipes/quickstart/finetuning/finetuning.py \
     --use_peft \
     --peft_method lora \
-    --model_name 'meta-llama/Llama-3.2-3B-Instruct' \
-    --output_dir './my_lora_weights/biotrip_json_no_quant_weight_decay' \
+    --model_name 'meta-llama/Llama-Llama-3.2-3B-Instruct' \
+    --output_dir './my_lora_weights/Llama-3.2-3B_biotrip_new_template' \
     --batch_size_training 1 \
     --batching_strategy "padding" \
     --weight_decay 0.2 \
     --num_epochs 6 \
     --dataset biotriplex_dataset \
-    --context_length 4150 \
+    --context_length 21000
+#    --quantization '4bit'
